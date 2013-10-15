2e506741d6c6c97fa3eaab32b6f8b15fca27dc97
Eduard Gamiao <edgamiao@gmail.com>
2013-10-15 08:37:37 -1000

    Initial Commit

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,6 +4,7 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.Index;
 import views.html.Page1;
+import views.html.NewContact;
 
 /**
  * Implements the controllers for this application.
@@ -24,6 +25,15 @@ public class Application extends Controller {
    */
   public static Result page1() {
     return ok(Page1.render("Welcome to Page1."));
+    
+  }
+  
+  /**
+   * Returns new contact, a page for new contacts.
+   * @return The newcontact.
+   */
+  public static Result newContact() {
+    return ok(NewContact.render("Welcome to Page1."));
     
   }
 }
