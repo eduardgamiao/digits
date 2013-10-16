c3f370bdc6d3b713d0073265415c8ac90f5a3986
Eduard Gamiao <edgamiao@gmail.com>
2013-10-15 16:21:34 -1000

    WOD 3: Form Validation
    Added validation to the form.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -13,30 +13,38 @@ import views.html.NewContact;
 public class Application extends Controller {
 
   /**
-   * Returns the home page. 
-   * @return The resulting home page. 
+   * Returns the home page.
+   * 
+   * @return The resulting home page.
    */
   public static Result index() {
     return ok(Index.render("Welcome to the home page."));
   }
-  
+
   /**
    * Returns new contact, a page for new contacts.
+   * 
    * @return The newcontact.
    */
   public static Result newContact() {
     Form<ContactFormData> formData = Form.form(ContactFormData.class);
     return ok(NewContact.render(formData));
   }
-  
+
   /**
    * Returns new contact, a page for new contacts.
+   * 
    * @return The newcontact.
    */
   public static Result postContact() {
     Form<ContactFormData> formData = Form.form(ContactFormData.class).bindFromRequest();
-    ContactFormData form = formData.get();
-    System.out.format("%s %s %n%s", form.firstName, form.lastName, form.telephone);
-    return ok(NewContact.render(formData));
+    if (formData.hasErrors()) {
+      return badRequest(NewContact.render(formData));
+    }
+    else {
+      ContactFormData form = formData.get();
+      System.out.format("%s %s %n%s", form.firstName, form.lastName, form.telephone);
+      return ok(NewContact.render(formData));
+    }
   }
 }
--- app/views/formdata/ContactFormData.java
+++ app/views/formdata/ContactFormData.java
@@ -1,16 +1,46 @@
 package views.formdata;
 
+import java.util.ArrayList;
+import java.util.List;
+import play.data.validation.ValidationError;
+
 /**
  * Implements contact form data.
  */
 public class ContactFormData {
 
+  private static final int TELEPHONE_LENGTH = 12;
+
   /** First name. */
   public String firstName = "";
-  
+
   /** Last name. */
   public String lastName = "";
-  
+
   /** Telephone. */
   public String telephone = "";
+
+  /**
+   * Validation of form input.
+   * 
+   * @return List of errors.
+   */
+  public List<ValidationError> validate() {
+    ArrayList<ValidationError> errors = new ArrayList<ValidationError>();
+
+    if (firstName == null || firstName.length() == 0) {
+      errors.add(new ValidationError("firstName", "First name is required."));
+    }
+    if (lastName == null || lastName.length() == 0) {
+      errors.add(new ValidationError("lastName", "Last name is required."));
+    }
+    if (telephone == null || telephone.length() == 0) {
+      errors.add(new ValidationError("telephone", "Telephone is required."));
+    }
+    if (telephone.length() != TELEPHONE_LENGTH) {
+      errors.add(new ValidationError("telephone", "Telephone must be formatted as xxx-xxx-xxxx."));
+    }
+
+    return errors.isEmpty() ? null : errors;
+  }
 }
