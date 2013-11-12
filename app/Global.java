ae63c6544a55b12613ed0267814da650fc65472d
Eduard Gamiao <edgamiao@gmail.com>
2013-11-12 08:08:22 -1000

    Finished.

--- app/Global.java
+++ app/Global.java
@@ -1,4 +1,5 @@
 import models.ContactDB;
+import models.UserInfoDB;
 import play.Application;
 import play.GlobalSettings;
 import views.formdata.ContactFormData;
@@ -15,10 +16,11 @@ public class Global extends GlobalSettings {
    * @param app An applcation.
    */
   public void onStart(Application app) {
+    UserInfoDB.addUserInfo("John Smith", "smith@example.com", "password");
+    
     ContactDB.addContact(new ContactFormData("Eduard", "Gamiao", "111-111-1111", "1234567890123456789012345", "Home"));
     ContactDB.addContact(new ContactFormData("John", "Doe", "222-222-2222", "1234567890123456789012345", "Work"));
     ContactDB.addContact(new ContactFormData("Jane", "Doe", "333-333-3333", "1234567890123456789012345", "Mobile"));
-    ContactDB.addContact(new ContactFormData("The", "Person", "123-456-7890", "1234567890123456789012345", "Home"));
-    
+    ContactDB.addContact(new ContactFormData("The", "Person", "123-456-7890", "1234567890123456789012345", "Home")); 
   }
 }
