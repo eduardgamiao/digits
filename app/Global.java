1d4ed6edd62dad1f4703992f40cf1ae89ba200f8
Eduard Gamiao <edgamiao@gmail.com>
2013-11-11 18:36:46 -1000

    End WOD.

--- app/Global.java
+++ app/Global.java
@@ -17,10 +17,11 @@ public class Global extends GlobalSettings {
    */
   public void onStart(Application app) {
     ContactDB.addContact(new ContactFormData("Eduard", "Gamiao", "111-111-1111", "1234567890123456789012345", "Home"));
-    ContactDB.addContact(new ContactFormData("John", "Doe", "222-222-2222", "1234567890123456789012345", "Work"));
+    ContactDB.addContact(new ContactFormData("John", "Smith", "222-222-2222", "1234567890123456789012345", "Work"));
     ContactDB.addContact(new ContactFormData("Jane", "Doe", "333-333-3333", "1234567890123456789012345", "Mobile"));
     ContactDB.addContact(new ContactFormData("The", "Person", "123-456-7890", "1234567890123456789012345", "Home"));
     
     UserInfoDB.addUserInfo("John Smith", "smith@example.com", "password");
+    UserInfoDB.addUserInfo("Eduard Gamiao", "eduard@hawaii.edu", "password");
   }
 }
