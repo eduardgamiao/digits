94ed09213da7a9d7110842ac60b20660754db25a
Eduard Gamiao <edgamiao@gmail.com>
2013-11-12 16:42:33 -1000

    Finish WOD.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -24,7 +24,8 @@ public class Application extends Controller {
    * @return The resulting home page.
    */
   public static Result index() {
-    return ok(Index.render(ContactDB.getContacts(), "Index", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx())));
+    return ok(Index.render(ContactDB.getContacts(Secured.getUser(ctx())), 
+        "Index", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx())));
   }
 
   /**
@@ -33,10 +34,12 @@ public class Application extends Controller {
    * @return The newcontact.
    */
   public static Result newContact(long id) {
-    ContactFormData data = (id == 0) ? new ContactFormData() : new ContactFormData(ContactDB.getContact(id));
+    ContactFormData data = (id == 0) ? new ContactFormData() 
+      : new ContactFormData(ContactDB.getContact(Secured.getUser(ctx()), id));
     Form<ContactFormData> formData = Form.form(ContactFormData.class).fill(data);
     Map<String, Boolean> telephoneTypeMap = TelephoneTypes.getTypes(data.telephoneType);    
-    return ok(NewContact.render(formData, telephoneTypeMap, "New", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx())));
+    return ok(NewContact.render(formData, telephoneTypeMap, 
+        "New", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx())));
   }
 
   /**
@@ -48,13 +51,15 @@ public class Application extends Controller {
     Form<ContactFormData> formData = Form.form(ContactFormData.class).bindFromRequest();
     if (formData.hasErrors()) {
       Map<String, Boolean> telephoneTypeMap = TelephoneTypes.getTypes();
-      return badRequest(NewContact.render(formData, telephoneTypeMap, "New", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx())));
+      return badRequest(NewContact.render(formData, telephoneTypeMap, 
+          "New", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx())));
     }
     else {
       ContactFormData form = formData.get();
-      ContactDB.addContact(form);
+      ContactDB.addContact(Secured.getUser(ctx()), form);
       Map<String, Boolean> telephoneTypeMap = TelephoneTypes.getTypes(form.telephoneType);      
-      return ok(NewContact.render(formData, telephoneTypeMap, "New", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx())));
+      return ok(NewContact.render(formData, telephoneTypeMap, 
+          "New", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx())));
     }
   }
   
@@ -64,8 +69,9 @@ public class Application extends Controller {
    * @return Index page after deletion.
    */
   public static Result deleteContact(long id) {
-    ContactDB.deleteContact(id);
-    return ok(Index.render(ContactDB.getContacts(), "Index", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx())));
+    ContactDB.deleteContact(Secured.getUser(ctx()), id);
+    return ok(Index.render(ContactDB.getContacts(Secured.getUser(ctx())), 
+        "Index", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx())));
   }
   
   /**
