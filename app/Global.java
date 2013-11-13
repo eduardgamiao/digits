9aecb3edd066e45b687366012d2fbd2252daf2c1
Eduard Gamiao <edgamiao@gmail.com>
2013-11-12 15:55:17 -1000

    Finish WOD.

--- app/Global.java
+++ app/Global.java
@@ -1,4 +1,5 @@
 import models.ContactDB;
+import models.UserInfoDB;
 import play.Application;
 import play.GlobalSettings;
 import views.formdata.ContactFormData;
@@ -18,6 +19,8 @@ public class Global extends GlobalSettings {
     ContactDB.addContact(new ContactFormData("Eduard", "Gamiao", "111-111-1111", "1234567890123456789012345", "Home"));
     ContactDB.addContact(new ContactFormData("John", "Doe", "222-222-2222", "1234567890123456789012345", "Work"));
     ContactDB.addContact(new ContactFormData("Jane", "Doe", "333-333-3333", "1234567890123456789012345", "Mobile"));
-    ContactDB.addContact(new ContactFormData("The", "Person", "123-456-7890", "1234567890123456789012345", "Home"));  
+    ContactDB.addContact(new ContactFormData("The", "Person", "123-456-7890", "1234567890123456789012345", "Home"));
+    
+    UserInfoDB.addUserInfo("John Smith", "smith@example.com", "password");
   }
 }
